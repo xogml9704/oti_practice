1e3c4fae02c122d0e8c9857d95a4991559cbb2bd
KOSA <KOSA@DESKTOP-2NFBL46>
2022-08-22 21:08:36 +0900

    example

---
+++ thisisjava/src/ch01/sec11/Hello.java
@@ -0,0 +1,21 @@
+package ch01.sec11;
+
+public class Hello {
+	
+	/**
+	 * @author 황건희
+	 * @param args
+	 */
+	
+	/*
+	 장제목 : 1장 자바 시작하기
+	 작성일 : 2022.08.22
+	 */
+	
+	// 프로그램 실행 진입점. main 메소드
+	public static void main(String[] args) {
+		// 출력하는 코드
+		System.out.println("Hello,Java2");
+	}
+
+}
