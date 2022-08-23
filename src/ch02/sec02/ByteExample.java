7b8b87475402bc5c54a632dbeb496c315489c11e
xogml9704 <xogml9704@naver.com>
2022-08-23 11:01:59 +0900

    practice

--- src/ch02/sec02/ByteExample.java
+++ src/ch02/sec02/ByteExample.java
@@ -7,13 +7,13 @@ public class ByteExample {
 		byte var3 = 0;
 		byte var4 = 30;
 		byte var5 = 127;
-		byte var6 = 128; // 컴파일 에러(Type mismatch : cannot covert from int to byte)
+		// byte var6 = 128; // 컴파일 에러(Type mismatch : cannot covert from int to byte)
 		
 		System.out.println(var1);
 		System.out.println(var2);
 		System.out.println(var3);
 		System.out.println(var4);
 		System.out.println(var5);
-		System.out.println(var6);
+		// System.out.println(var6);
 	}
 }
---
+++ src/ch02/sec06/StringExample.java
@@ -0,0 +1,27 @@
+package ch02.sec06;
+
+public class StringExample {
+	public static void main(String[] args) {
+		String name = "김태희";
+		String job = "프로그래머";
+		System.out.println(name);
+		System.out.println(job);
+		
+		String str = "나는 \"자바\"를 배웁니다.";
+		
+		System.out.println(str);
+		
+		str = "번호\t이름\t직업";
+		
+		System.out.println(str);
+		
+		System.out.print("나는\n");
+		System.out.print("자바를\n");
+		System.out.print("배웁니다.\n");
+		
+		System.out.print("나는 ");
+		System.out.print("자바를 ");
+		System.out.print("배웁니다.");
+		
+	}
+}
