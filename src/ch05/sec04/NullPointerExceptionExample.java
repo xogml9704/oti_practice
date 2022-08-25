f407f002fae01462d5174900924043171868ab8f
xogml9704 <xogml9704@naver.com>
2022-08-25 12:21:48 +0900

    example

---
+++ src/ch05/sec04/NullPointerExceptionExample.java
@@ -0,0 +1,11 @@
+package ch05.sec04;
+
+public class NullPointerExceptionExample {
+	public static void main(String[] args) {
+		int[] intArry = null;
+		// intArry[0] = 10;
+		
+		String str = null;
+		// System.out.println("str 문자의 길이 : " + str.length());
+	}
+}
