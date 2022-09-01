2c1c7762ab2d5e394cf57606486593b53338e94c
xogml9704 <xogml9704@naver.com>
2022-09-01 20:06:07 +0900

    practcie

--- src/ch06/sec07/exam01/Car.java
+++ src/ch06/sec07/exam01/Car.java
@@ -2,8 +2,6 @@ package ch06.sec07.exam01;
 
 public class Car {
 	Car(String model, String color, int maxSpeed) {
-		if(model.equals("그렌져")) {
-			model = "차";
-		}
+		
 	}
 }
--- src/ch06/sec07/exam01/CarExample.java
+++ src/ch06/sec07/exam01/CarExample.java
@@ -2,13 +2,8 @@ package ch06.sec07.exam01;
 
 public class CarExample {
 	public static void main(String[] args) {
-		// Car myCar = new Car();
 		
-		Car myCar2 = new Car("그렌져", "검정", 250);
-		
-		// Car myCar3 = new Car("그렌져", "검정", 250, "그린");
-		
-		System.out.println(myCar2.equals("그렌져"));
+		Car myCar = new Car("그렌져", "검정", 250);
 	
 	}
 }
