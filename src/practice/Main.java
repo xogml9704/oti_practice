367477b767ba6fab789b7a1352f0f0ccc16ec4dd
xogml9704 <xogml9704@naver.com>
2022-09-15 17:45:24 +0900

    practice

---
+++ src/practice/Main.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Main {
+	String nation = "대한민국";
+	
+	void method2() {
+		nation = "한국";
+	}
+}
---
+++ src/practice/MainExample.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class MainExample {
+	public static void main(String[] args) {
+		Main m = new Main();
+		System.out.println(m.nation);
+		m.method2();
+		System.out.println(m.nation);
+	}
+}
