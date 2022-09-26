6a6e1b4efd5a622b65c6ceeae7c2ff1c10eae04d
xogml9704 <xogml9704@naver.com>
2022-09-26 20:33:02 +0900

    practcie

--- src/practice/MainExample.java
+++ src/practice/MainExample.java
@@ -1,20 +1,7 @@
 package practice;
 
-import java.util.TreeSet;
-
 public class MainExample {
 	public static void main(String[] args) {
-		// 비교자를 제공한 TreeSet 컬렉션 생성
-		TreeSet<Main> treeSet = new TreeSet<Main>(new MainComparator());
 		
-		// 객체 저장
-		treeSet.add(new Main("포도", 3000));
-		treeSet.add(new Main("수박", 10000));
-		treeSet.add(new Main("딸기", 6000));
-		
-		// 객체를 하나씩 가져오기
-		for(Main main : treeSet) {
-			System.out.println(main.name + " : " + main.price);
-		}
 	}
 }
