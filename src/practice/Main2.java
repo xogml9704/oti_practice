05c4e5ae66519f24b5b3f7fb8abcdb2e239d84b7
xogml9704 <xogml9704@naver.com>
2022-10-21 17:40:24 +0900

    practcie

--- src/practice/Main2.java
+++ src/practice/Main2.java
@@ -1,11 +1,21 @@
 package practice;
 
-public class Main2 extends Main {
-    // 필드 선언
-    public String filed2;
-    
-    // 메소드 선언
-    public void method3() {
-        System.out.println("Child-method3()");
+public class Main2 {
+    public static void main(String arg[]) {
+
+        
+        
+        
     }
+    static int run(int n ) {
+        if( n==0 ) {
+            return 1;
+        } else if( n== 1) {
+            return 0;
+        } else
+        
+        return 0;
+    }
+    
+    
 }
